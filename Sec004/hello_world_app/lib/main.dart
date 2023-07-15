88efa05c60790698308b86cf571864d7da2479dd
Jose Alonso <mail@josealonso.dev>
2023-07-14 23:46:06 -0600

    Adding a new screen

--- Sec004/hello_world_app/lib/main.dart
+++ Sec004/hello_world_app/lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:hello_world_app/presentation/screens/counters/counter_screen.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -10,9 +11,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Scaffold(
-          body: Center(child: Text('Hola Mundo')),
-        ));
+        debugShowCheckedModeBanner: false, home: CounterScreen());
   }
 }
