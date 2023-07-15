5de851d1292b340b2d1adc7c5acea73c59801f88
José Alonso <mail@josealonso.dev>
2023-07-15 17:37:53 -0600

    Textfield behavior

--- Sec005/yes_no_app/lib/presentation/widgets/shared/message_field.box.dart
+++ Sec005/yes_no_app/lib/presentation/widgets/shared/message_field.box.dart
@@ -5,29 +5,39 @@ class MessageFieldBox extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final textController = TextEditingController();
+    final focusNode = FocusNode();
+
     final outlineInputBorder = UnderlineInputBorder(
         borderSide: const BorderSide(color: Colors.transparent),
         borderRadius: BorderRadius.circular(25));
 
     final inputDecoration = InputDecoration(
+      hintText: 'End your message with a "?"',
       enabledBorder: outlineInputBorder,
       focusedBorder: outlineInputBorder,
       filled: true,
       suffix: IconButton(
         icon: const Icon(Icons.send_outlined),
         onPressed: () {
-          print('Valor de la cada de texto');
+          final textValue = textController.value.text;
+          print('Button: $textValue');
+          textController.clear();
         },
       ),
     );
 
     return TextFormField(
+      onTapOutside: (event) {
+        focusNode.unfocus();
+      },
+      focusNode: focusNode,
+      controller: textController,
       decoration: inputDecoration,
       onFieldSubmitted: (value) {
         print('Submit value $value');
-      },
-      onChanged: (value) {
-        print('changed: $value');
+        textController.clear();
+        focusNode.requestFocus();
       },
     );
   }
