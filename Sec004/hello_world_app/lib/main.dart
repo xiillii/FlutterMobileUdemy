4f503363ea7dc6a6483a5b1719bc9bd093f25b58
Jose Alonso <mail@josealonso.dev>
2023-07-15 00:42:27 -0600

    Change the counter screen and use Material 3 theme

--- Sec004/hello_world_app/lib/main.dart
+++ Sec004/hello_world_app/lib/main.dart
@@ -10,7 +10,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-        debugShowCheckedModeBanner: false, home: CounterScreen());
+    return MaterialApp(
+        theme:
+            ThemeData(useMaterial3: true, colorSchemeSeed: Colors.limeAccent),
+        debugShowCheckedModeBanner: false,
+        home: const CounterScreen());
   }
 }
--- Sec004/hello_world_app/lib/presentation/screens/counters/counter_screen.dart
+++ Sec004/hello_world_app/lib/presentation/screens/counters/counter_screen.dart
@@ -5,8 +5,29 @@ class CounterScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('Counter Screen')),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Counter Screen'),
+      ),
+      body: const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              '10',
+              style: TextStyle(fontSize: 160, fontWeight: FontWeight.w100),
+            ),
+            Text(
+              'Clicks',
+              style: TextStyle(fontSize: 25),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.plus_one),
+      ),
     );
   }
 }
