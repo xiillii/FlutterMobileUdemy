8605295ed15a203e81a8a413d517096cd20a34b8
Jose Alonso <mail@josealonso.dev>
2023-07-15 00:56:01 -0600

    Perform the Click onPressed

--- Sec004/hello_world_app/lib/presentation/screens/counters/counter_screen.dart
+++ Sec004/hello_world_app/lib/presentation/screens/counters/counter_screen.dart
@@ -1,23 +1,31 @@
 import 'package:flutter/material.dart';
 
-class CounterScreen extends StatelessWidget {
+class CounterScreen extends StatefulWidget {
   const CounterScreen({super.key});
+
+  @override
+  State<CounterScreen> createState() => _CounterScreenState();
+}
+
+class _CounterScreenState extends State<CounterScreen> {
+  int clickCounter = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Counter Screen'),
+        title: const Text('Counter Screen'),
       ),
-      body: const Center(
+      body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text(
-              '10',
-              style: TextStyle(fontSize: 160, fontWeight: FontWeight.w100),
+              '$clickCounter',
+              style:
+                  const TextStyle(fontSize: 160, fontWeight: FontWeight.w100),
             ),
-            Text(
+            const Text(
               'Clicks',
               style: TextStyle(fontSize: 25),
             ),
@@ -25,7 +33,10 @@ class CounterScreen extends StatelessWidget {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          clickCounter++;
+          setState(() {});
+        },
         child: const Icon(Icons.plus_one),
       ),
     );
