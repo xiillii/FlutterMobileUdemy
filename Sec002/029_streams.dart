ecb61bdcbfc1af73f44adc619c4b316fa2d7ea0a
Jose Alonso <mail@josealonso.dev>
2023-07-14 22:54:51 -0600

    Create 029_streams.dart

---
+++ Sec002/029_streams.dart
@@ -0,0 +1,11 @@
+void main() {
+  emitNumbers().listen((value) {
+    print('Stream value: $value');
+  });
+}
+
+Stream<int> emitNumbers() {
+  return Stream.periodic(const Duration(seconds: 1), (value) {
+    return value;
+  }).take(5);
+}
