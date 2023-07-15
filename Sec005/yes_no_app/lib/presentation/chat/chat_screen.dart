b7871a4f23f2a256b5fc86ec19ecffa2dd1c9612
José Alonso <mail@josealonso.dev>
2023-07-15 14:23:36 -0600

    ListView for the Chat Screen

--- Sec005/yes_no_app/lib/presentation/chat/chat_screen.dart
+++ Sec005/yes_no_app/lib/presentation/chat/chat_screen.dart
@@ -17,6 +17,29 @@ class ChatScreen extends StatelessWidget {
         title: const Text('Mi Amuchis 😘'),
         centerTitle: false,
       ),
+      body: _ChatView(),
+    );
+  }
+}
+
+class _ChatView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          children: [
+            Expanded(
+                child: ListView.builder(
+                    itemCount: 100,
+                    itemBuilder: (context, index) {
+                      return Text('Indice: $index');
+                    })),
+            Text('Mundo')
+          ],
+        ),
+      ),
     );
   }
 }
