8e39b4565baf4838ff85855fc5aaac09fef86e60
Jose Alonso <mail@josealonso.dev>
2023-07-15 22:42:31 -0600

    Emulate her reply consuming the API Yes No yesno.wtf

--- Sec005/yes_no_app/lib/presentation/providers/chat_provider.dart
+++ Sec005/yes_no_app/lib/presentation/providers/chat_provider.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:yes_no_app/config/helpers/get_yes_no_answer.dart';
 import 'package:yes_no_app/domain/entities/message.dart';
 
 class ChatProvider extends ChangeNotifier {
   final ScrollController chatScrollController = ScrollController();
+  final GetYesNoAnswer getYesNoAnswer = GetYesNoAnswer();
 
   List<Message> messages = [
     Message(text: 'Hola amor', fromWho: FromWho.me),
@@ -17,6 +19,14 @@ class ChatProvider extends ChangeNotifier {
 
     notifyListeners();
     moveScrollToBottom();
+
+    if (text.endsWith('?')) {
+      herReply();
+    }
+  }
+
+  Future<void> herReply() async {
+    final herMessage = await getYesNoAnswer.getAnswer();
   }
 
   Future<void> moveScrollToBottom() async {
