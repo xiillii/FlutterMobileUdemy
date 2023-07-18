908fec1a83c7c3072e8eefbd43843baf968cc446
Jose Alonso <mail@josealonso.dev>
2023-07-17 20:27:59 -0600

    TokTik: Load video assets

--- Sec007/toktik/lib/main.dart
+++ Sec007/toktik/lib/main.dart
@@ -12,7 +12,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
-      providers: [ChangeNotifierProvider(create: (_) => DiscoverProvider())],
+      providers: [
+        ChangeNotifierProvider(
+            create: (_) => DiscoverProvider()..loadNextPage())
+      ],
       child: MaterialApp(
         title: 'TokTik',
         debugShowCheckedModeBanner: false,
