dd191fb92c41471910162217377f66707a30b58c
Jose Alonso <mail@josealonso.dev>
2023-07-18 21:23:01 -0600

    TokTik: Play videos

--- Sec007/toktik/lib/presentation/widgets/video/fullscreen_player.dart
+++ Sec007/toktik/lib/presentation/widgets/video/fullscreen_player.dart
@@ -36,12 +36,48 @@ class _FullscreenPlayerState extends State<FullscreenPlayer> {
     return FutureBuilder(
       future: controller.initialize(),
       builder: (context, snapshot) {
-        return const Center(
-          child: CircularProgressIndicator(
-            strokeWidth: 2,
-          ),
+        if (snapshot.connectionState != ConnectionState.done) {
+          return const Center(
+            child: CircularProgressIndicator(
+              strokeWidth: 2,
+            ),
+          );
+        }
+        return AspectRatio(
+          aspectRatio: controller.value.aspectRatio,
+          child: Stack(children: [
+            VideoPlayer(controller),
+
+            // Text
+            Positioned(
+                bottom: 50,
+                left: 20,
+                child: _VideoCaption(
+                  caption: widget.caption,
+                ))
+          ]),
         );
       },
+    );
+  }
+}
+
+class _VideoCaption extends StatelessWidget {
+  final String caption;
+  const _VideoCaption({super.key, required this.caption});
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    final titleStyle = Theme.of(context).textTheme.titleLarge;
+
+    return SizedBox(
+      width: size.width * 0.6,
+      child: Text(
+        caption,
+        maxLines: 2,
+        style: titleStyle,
+      ),
     );
   }
 }
