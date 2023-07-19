1f2d3e0b6edc87b947d80c0d5a11686580679cfc
Jose Alonso <mail@josealonso.dev>
2023-07-18 20:15:08 -0600

    Spin play icon

--- Sec007/toktik/lib/presentation/widgets/shared/video_buttons.dart
+++ Sec007/toktik/lib/presentation/widgets/shared/video_buttons.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'package:toktik/config/helpers/human_formats.dart';
 import 'package:toktik/domain/entities/video_post.dart';
@@ -12,9 +13,23 @@ class VideoButtons extends StatelessWidget {
     return Column(children: [
       _CustomIconButton(
           value: video.likes, iconData: Icons.favorite, iconColor: Colors.red),
+      const SizedBox(
+        height: 20,
+      ),
       _CustomIconButton(
         value: video.views,
         iconData: Icons.remove_red_eye_outlined,
+      ),
+      const SizedBox(
+        height: 20,
+      ),
+      SpinPerfect(
+        infinite: true,
+        duration: const Duration(seconds: 5),
+        child: const _CustomIconButton(
+          value: 0,
+          iconData: Icons.play_circle_outline,
+        ),
       ),
     ]);
   }
@@ -39,7 +54,7 @@ class _CustomIconButton extends StatelessWidget {
             color: color,
             size: 30,
           )),
-      Text(HumanFormats.humanReadableNumber(value.toDouble()))
+      if (value > 0) Text(HumanFormats.humanReadableNumber(value.toDouble()))
     ]);
   }
 }
