8ddc57716c53f80c796707362608ca6cd9b22965
Jose Alonso <mail@josealonso.dev>
2023-07-18 18:08:59 -0600

    CircularProgress and PageView

--- Sec007/toktik/lib/presentation/screens/discover/discover_screen.dart
+++ Sec007/toktik/lib/presentation/screens/discover/discover_screen.dart
@@ -1,12 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:toktik/presentation/providers/discover_provider.dart';
+
+import '../../widgets/shared/video_scrollable_view.dart';
 
 class DiscoverScreen extends StatelessWidget {
   const DiscoverScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('Discover Screen')),
-    );
+    final discoverProvider = context.watch<DiscoverProvider>();
+
+    discoverProvider.initialLoading = false;
+
+    return Scaffold(
+        body: discoverProvider.initialLoading
+            ? const Center(
+                child: CircularProgressIndicator(
+                  strokeWidth: 2,
+                ),
+              )
+            : VideoScrollableView(
+                videos: discoverProvider.videos,
+              ));
   }
 }
