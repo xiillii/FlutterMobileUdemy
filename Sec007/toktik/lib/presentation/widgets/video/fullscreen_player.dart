4a2e25456ba2036580720c6fa713458bbe1b1025
Jose Alonso <mail@josealonso.dev>
2023-07-18 21:05:10 -0600

    TokTik: Video controller  and FutureBuilders

--- Sec007/toktik/lib/presentation/widgets/video/fullscreen_player.dart
+++ Sec007/toktik/lib/presentation/widgets/video/fullscreen_player.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
 
-class FullscreenPlayer extends StatelessWidget {
+class FullscreenPlayer extends StatefulWidget {
   final String videoUrl;
   final String caption;
 
@@ -8,7 +9,39 @@ class FullscreenPlayer extends StatelessWidget {
       {super.key, required this.videoUrl, required this.caption});
 
   @override
+  State<FullscreenPlayer> createState() => _FullscreenPlayerState();
+}
+
+class _FullscreenPlayerState extends State<FullscreenPlayer> {
+  late VideoPlayerController controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    controller = VideoPlayerController.asset(widget.videoUrl)
+      ..setVolume(0)
+      ..setLooping(true)
+      ..play();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return FutureBuilder(
+      future: controller.initialize(),
+      builder: (context, snapshot) {
+        return const Center(
+          child: CircularProgressIndicator(
+            strokeWidth: 2,
+          ),
+        );
+      },
+    );
   }
 }
