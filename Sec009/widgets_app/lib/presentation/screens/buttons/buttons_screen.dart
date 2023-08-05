0b9719f73d97d7b4266f817f6cd1af9d75f7e995
Jose Alonso <mail@josealonso.dev>
2023-08-04 23:07:03 -0600

    Widgets App: Diferentes botones preconfigurados

--- Sec009/widgets_app/lib/presentation/screens/buttons/buttons_screen.dart
+++ Sec009/widgets_app/lib/presentation/screens/buttons/buttons_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class ButtonsScreen extends StatelessWidget {
   static const String name = 'buttons_screen';
@@ -9,7 +10,105 @@ class ButtonsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Buttons Screen')),
-      body: const Placeholder(),
+      body: const _ButtonsView(),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.arrow_back_ios_new_rounded),
+        onPressed: () {
+          context.pop();
+        },
+      ),
+    );
+  }
+}
+
+class _ButtonsView extends StatelessWidget {
+  const _ButtonsView();
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
+    return SizedBox(
+      width: double.infinity,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+        child: Wrap(
+          spacing: 10,
+          alignment: WrapAlignment.center,
+          children: [
+            ElevatedButton(onPressed: () {}, child: const Text('Elevated')),
+            const ElevatedButton(
+                onPressed: null, child: Text('Elevated Disabled')),
+            ElevatedButton.icon(
+                onPressed: () {},
+                icon: const Icon(Icons.add_a_photo_outlined),
+                label: const Text('Elevated Icon')),
+            ElevatedButton.icon(
+                onPressed: null,
+                icon: const Icon(Icons.add_a_photo_outlined),
+                label: const Text('Elevated Icon Disabled')),
+            FilledButton(onPressed: () {}, child: const Text('Filled')),
+            const FilledButton(onPressed: null, child: Text('Filled Disabled')),
+            FilledButton.icon(
+                onPressed: () {},
+                icon: const Icon(Icons.holiday_village),
+                label: const Text('Filled Icon')),
+            FilledButton.icon(
+                onPressed: null,
+                icon: const Icon(Icons.holiday_village),
+                label: const Text('Filled Icon Disabled')),
+            OutlinedButton(onPressed: () {}, child: const Text('Outlined')),
+            const OutlinedButton(
+                onPressed: null, child: Text('Outlined Disabled')),
+            OutlinedButton.icon(
+                onPressed: () {},
+                icon: const Icon(Icons.abc),
+                label: const Text('Outlined Icon')),
+            OutlinedButton.icon(
+                onPressed: null,
+                icon: const Icon(Icons.abc),
+                label: const Text('Outlined Icon Disabled')),
+            TextButton(onPressed: () {}, child: const Text('Text')),
+            const TextButton(onPressed: null, child: Text('Text Disabled')),
+            TextButton.icon(
+                onPressed: () {},
+                icon: const Icon(Icons.air_outlined),
+                label: const Text('Text Icon')),
+            TextButton.icon(
+                onPressed: null,
+                icon: const Icon(Icons.air_outlined),
+                label: const Text('Text Icon Disabled')),
+            IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.app_settings_alt,
+                )),
+            const IconButton(
+                onPressed: null,
+                icon: Icon(
+                  Icons.app_settings_alt,
+                )),
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.all_inbox_rounded,
+              ),
+              style: ButtonStyle(
+                  backgroundColor: MaterialStatePropertyAll(colors.primary),
+                  iconColor: const MaterialStatePropertyAll(Colors.white)),
+            ),
+            IconButton(
+              onPressed: null,
+              icon: const Icon(
+                Icons.all_inbox_rounded,
+              ),
+              style: ButtonStyle(
+                backgroundColor: MaterialStatePropertyAll(colors.primary),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
