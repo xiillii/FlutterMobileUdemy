ed7418614cb0ece8dfe5d24af8923e92782131ab
Jose Alonso <mail@josealonso.dev>
2023-08-05 00:04:04 -0600

    Widgets App: Tarjetas con borde

--- Sec009/widgets_app/lib/presentation/screens/cards/cards_screen.dart
+++ Sec009/widgets_app/lib/presentation/screens/cards/cards_screen.dart
@@ -34,7 +34,7 @@ class _CardsView extends StatelessWidget {
           ...cards.map(
               (e) => _CardType1(label: e['label'], elevation: e['elevation'])),
           ...cards.map(
-              (e) => _CardType1(label: e['label'], elevation: e['elevation'])),
+              (e) => _CardType2(label: e['label'], elevation: e['elevation'])),
         ],
       ),
     );
@@ -56,12 +56,45 @@ class _CardType1 extends StatelessWidget {
           Align(
               alignment: Alignment.topRight,
               child: IconButton(
-                icon: Icon(Icons.more_vert_outlined),
+                icon: const Icon(Icons.more_vert_outlined),
                 onPressed: () {},
               )),
           Align(
             alignment: Alignment.bottomLeft,
             child: Text(label),
+          )
+        ]),
+      ),
+    );
+  }
+}
+
+class _CardType2 extends StatelessWidget {
+  final String label;
+  final double elevation;
+  const _CardType2({required this.label, required this.elevation});
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
+    return Card(
+      shape: RoundedRectangleBorder(
+          borderRadius: const BorderRadius.all(Radius.circular(12)),
+          side: BorderSide(color: colors.outline)),
+      elevation: elevation,
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(10, 5, 10, 10),
+        child: Column(children: [
+          Align(
+              alignment: Alignment.topRight,
+              child: IconButton(
+                icon: const Icon(Icons.more_vert_outlined),
+                onPressed: () {},
+              )),
+          Align(
+            alignment: Alignment.bottomLeft,
+            child: Text('$label - outline'),
           )
         ]),
       ),
