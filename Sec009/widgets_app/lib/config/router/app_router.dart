771362bed980073f7ccd75988b88d1fc03b84555
Jose Alonso <mail@josealonso.dev>
2023-08-04 22:28:02 -0600

    Widgets App; Colocando nombres a las rutas

--- Sec009/widgets_app/lib/config/router/app_router.dart
+++ Sec009/widgets_app/lib/config/router/app_router.dart
@@ -7,14 +7,17 @@ final appRouter = GoRouter(
   routes: [
     GoRoute(
       path: '/',
+      name: HomeScreen.name,
       builder: (context, state) => const HomeScreen(),
     ),
     GoRoute(
       path: '/buttons',
+      name: ButtonsScreen.name,
       builder: (context, state) => const ButtonsScreen(),
     ),
     GoRoute(
       path: '/cards',
+      name: CardsScreen.name,
       builder: (context, state) => const CardsScreen(),
     ),
   ],
