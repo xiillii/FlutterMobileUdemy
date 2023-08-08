372c9c6e5efc7a74dbc3513bc6d5b502efe392d9
Jose Alonso <mail@josealonso.dev>
2023-08-07 23:14:10 -0600

    Widgets app: refresh indicator infinite images

--- Sec009/widgets_app/lib/presentation/screens/infinite_scroll/infinite_scroll_screen.dart
+++ Sec009/widgets_app/lib/presentation/screens/infinite_scroll/infinite_scroll_screen.dart
@@ -35,7 +35,7 @@ class _InfiniteScrollScreenState extends State<InfiniteScrollScreen> {
     super.dispose();
   }
 
-  Future loadNextPage() async {
+  Future<void> loadNextPage() async {
     if (isLoading) {
       return;
     }
@@ -57,6 +57,27 @@ class _InfiniteScrollScreenState extends State<InfiniteScrollScreen> {
     // TODO: mover scroll
   }
 
+  Future<void> onRefresh() async {
+    isLoading = true;
+    setState(() {});
+
+    await Future.delayed(const Duration(seconds: 3));
+
+    if (!isMounted) {
+      return;
+    }
+    isLoading = false;
+
+    final lastId = imagesIds.last;
+
+    imagesIds.clear();
+    imagesIds.add(lastId + 1);
+
+    addFiveImages();
+
+    setState(() {});
+  }
+
   void addFiveImages() {
     final lastId = imagesIds.last;
 
@@ -71,19 +92,24 @@ class _InfiniteScrollScreenState extends State<InfiniteScrollScreen> {
         context: context,
         removeTop: true,
         removeBottom: true,
-        child: ListView.builder(
-            controller: scrollController,
-            itemCount: imagesIds.length,
-            itemBuilder: (context, index) {
-              return FadeInImage(
-                  fit: BoxFit.cover,
-                  width: double.infinity,
-                  height: 300,
-                  placeholder:
-                      const AssetImage('assets/images/jar-loading.gif'),
-                  image: NetworkImage(
-                      'https://picsum.photos/id/${imagesIds[index]}/500/300'));
-            }),
+        child: RefreshIndicator(
+          onRefresh: onRefresh,
+          edgeOffset: 10,
+          strokeWidth: 2,
+          child: ListView.builder(
+              controller: scrollController,
+              itemCount: imagesIds.length,
+              itemBuilder: (context, index) {
+                return FadeInImage(
+                    fit: BoxFit.cover,
+                    width: double.infinity,
+                    height: 300,
+                    placeholder:
+                        const AssetImage('assets/images/jar-loading.gif'),
+                    image: NetworkImage(
+                        'https://picsum.photos/id/${imagesIds[index]}/500/300'));
+              }),
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () => context.pop(),
