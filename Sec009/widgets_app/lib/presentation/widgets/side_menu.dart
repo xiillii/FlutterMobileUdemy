a4ae8b12810a593bbf6e8a4db024f1d89cc3699c
Jose Alonso <mail@josealonso.dev>
2023-08-07 23:51:35 -0600

    Widgets App: Considerar el notch y colocar las opciones de menu

--- Sec009/widgets_app/lib/presentation/widgets/side_menu.dart
+++ Sec009/widgets_app/lib/presentation/widgets/side_menu.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:widgets_app/config/menu/menu_item.dart';
 
 class SideMenu extends StatefulWidget {
   const SideMenu({super.key});
@@ -12,6 +13,8 @@ class _SideMenuState extends State<SideMenu> {
 
   @override
   Widget build(BuildContext context) {
+    final hasNotch = MediaQuery.of(context).viewPadding.top > 35;
+
     return NavigationDrawer(
       selectedIndex: navDrawerIndex,
       onDestinationSelected: (value) {
@@ -20,11 +23,24 @@ class _SideMenuState extends State<SideMenu> {
         });
       },
       children: [
-        NavigationDrawerDestination(
-            icon: const Icon(Icons.add), label: const Text('Home Screen')),
-        NavigationDrawerDestination(
-            icon: const Icon(Icons.add_shopping_cart_rounded),
-            label: const Text('Otra pantalla')),
+        Padding(
+          padding: EdgeInsets.fromLTRB(28, hasNotch ? 0 : 20, 16, 10),
+          child: const Text('Main'),
+        ),
+        ...appMenuItems.sublist(0, 3).map(
+              (item) => NavigationDrawerDestination(
+                  icon: Icon(item.icon), label: Text(item.title)),
+            ),
+        const Padding(
+          padding: EdgeInsets.fromLTRB(28, 16, 28, 10),
+          child: Divider(),
+        ),
+        const Padding(
+          padding: EdgeInsets.fromLTRB(28, 16, 28, 10),
+          child: Text('More options'),
+        ),
+        ...appMenuItems.sublist(3).map((e) => NavigationDrawerDestination(
+            icon: Icon(e.icon), label: Text(e.title)))
       ],
     );
   }
