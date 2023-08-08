707f8b52a6d8f9f5f67b08b766d0a4f69b42d6f6
Jose Alonso <mail@josealonso.dev>
2023-08-07 23:03:53 -0600

    Widgets App: Infinite images, show loading indicator

--- Sec009/widgets_app/lib/presentation/screens/infinite_scroll/infinite_scroll_screen.dart
+++ Sec009/widgets_app/lib/presentation/screens/infinite_scroll/infinite_scroll_screen.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
@@ -52,6 +53,8 @@ class _InfiniteScrollScreenState extends State<InfiniteScrollScreen> {
       return;
     }
     setState(() {});
+
+    // TODO: mover scroll
   }
 
   void addFiveImages() {
@@ -84,7 +87,12 @@ class _InfiniteScrollScreenState extends State<InfiniteScrollScreen> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () => context.pop(),
-        child: const Icon(Icons.arrow_back_ios_new_outlined),
+        child: isLoading
+            ? SpinPerfect(
+                infinite: true,
+                child: const Icon(Icons.refresh_outlined),
+              )
+            : FadeIn(child: const Icon(Icons.arrow_back_ios_new_outlined)),
       ),
     );
   }
