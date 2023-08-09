1f759110c8eef0592223780677a640a2f8a9a403
Jose Alonso <mail@josealonso.dev>
2023-08-08 19:16:57 -0600

    Widgets App: preparando para cambiar darkmode con riverpod provider

--- Sec009/widgets_app/lib/presentation/screens/counter/counter_screen.dart
+++ Sec009/widgets_app/lib/presentation/screens/counter/counter_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:widgets_app/presentation/providers/counter_provider.dart';
+import 'package:widgets_app/presentation/providers/theme_provider.dart';
 
 class CounterScreen extends ConsumerWidget {
   static const name = 'counter_screen';
@@ -9,9 +10,21 @@ class CounterScreen extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final int clickCounter = ref.watch(counterProvider);
+    final bool isDarkMode = ref.watch(isDarkModeProvider);
 
     return Scaffold(
-      appBar: AppBar(title: const Text('Counter Screen')),
+      appBar: AppBar(
+        title: const Text('Counter Screen'),
+        actions: [
+          IconButton(
+              onPressed: () {
+                ref.read(isDarkModeProvider.notifier).update((state) => !state);
+              },
+              icon: Icon(isDarkMode
+                  ? Icons.dark_mode_outlined
+                  : Icons.light_mode_outlined))
+        ],
+      ),
       body: Center(
         child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
           Text(
@@ -21,7 +34,10 @@ class CounterScreen extends ConsumerWidget {
         ]),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          // ref.read(counterProvider.notifier).state++;
+          ref.read(counterProvider.notifier).update((state) => state + 1);
+        },
         child: const Icon(Icons.add_outlined),
       ),
     );
