5309eaa9ae2d7f2f00c39962b96082f3326f2460
Jose Alonso <mail@josealonso.dev>
2023-08-08 18:49:07 -0600

    Widgets App: Usando riverpod

--- Sec009/widgets_app/lib/presentation/screens/counter/counter_screen.dart
+++ Sec009/widgets_app/lib/presentation/screens/counter/counter_screen.dart
@@ -1,17 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:widgets_app/presentation/providers/counter_provider.dart';
 
-class CounterScreen extends StatelessWidget {
+class CounterScreen extends ConsumerWidget {
   static const name = 'counter_screen';
   const CounterScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final int clickCounter = ref.watch(counterProvider);
+
     return Scaffold(
       appBar: AppBar(title: const Text('Counter Screen')),
       body: Center(
         child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
           Text(
-            'Valor: 10',
+            'Valor: $clickCounter',
             style: Theme.of(context).textTheme.titleLarge,
           )
         ]),
