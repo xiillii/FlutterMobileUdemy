5ec64f7eef228e08d093e310b53fc78885780d0c
Jose Alonso <mail@josealonso.dev>
2023-08-09 22:48:44 -0600

    Cinemapedia: Get the movies playing with DIO library

--- Sec012/cinemapedia/lib/presentation/screens/movies/home_screen.dart
+++ Sec012/cinemapedia/lib/presentation/screens/movies/home_screen.dart
@@ -9,9 +9,7 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text(Environment.theMovieDbKey),
-      ),
+      body: const Placeholder(),
     );
   }
 }
