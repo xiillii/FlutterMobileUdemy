90782f5053e6949c8bf5243e92f2c1780a8e9a7c
Jose Alonso <mail@josealonso.dev>
2023-08-10 20:07:25 -0600

    Cinemapedia: Mapping the movie response Now_Playing

--- Sec012/cinemapedia/lib/infrastructure/datasources/moviedb_datasource.dart
+++ Sec012/cinemapedia/lib/infrastructure/datasources/moviedb_datasource.dart
@@ -1,6 +1,9 @@
 import 'package:cinemapedia/config/constants/environment.dart';
+import 'package:cinemapedia/config/constants/general.dart';
 import 'package:cinemapedia/domain/datasources/movies_datasource.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/infrastructure/mappers/movie_mapper.dart';
+import 'package:cinemapedia/infrastructure/models/moviedb/moviedb_response.dart';
 import 'package:dio/dio.dart';
 
 class MovieDbDatasource extends MoviesDatasource {
@@ -13,7 +16,11 @@ class MovieDbDatasource extends MoviesDatasource {
   @override
   Future<List<Movie>> getNowPlaying({int page = 1}) async {
     final response = await dio.get('/movie/now_playing');
-    final List<Movie> movies = [];
+    final movieDbResponse = MovieDbResponse.fromJson(response.data);
+    final List<Movie> movies = movieDbResponse.results
+        .where((element) => element.posterPath != GeneralConstants.noPoster)
+        .map((e) => MovieMapper.movieDbToEntity(e))
+        .toList();
 
     return movies;
   }
