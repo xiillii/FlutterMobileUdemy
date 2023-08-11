a716e869ecbfe3ceb621b36cf8028976afb97943
Jose Alonso <mail@josealonso.dev>
2023-08-10 23:45:40 -0600

    Cinemapedia: Show backdrop image movie in the swiper

--- Sec012/cinemapedia/lib/presentation/widgets/movies/movies_slideshow.dart
+++ Sec012/cinemapedia/lib/presentation/widgets/movies/movies_slideshow.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:card_swiper/card_swiper.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:flutter/material.dart';
@@ -34,6 +35,33 @@ class _Slide extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Placeholder();
+    final decoration = BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        boxShadow: const [
+          BoxShadow(
+              color: Colors.black45, blurRadius: 10, offset: Offset(0, 10))
+        ]);
+
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 30),
+      child: DecoratedBox(
+        decoration: decoration,
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(20),
+          child: Image.network(
+            movie.backdropPath,
+            fit: BoxFit.cover,
+            loadingBuilder: (context, child, loadingProgress) {
+              if (loadingProgress != null) {
+                return const DecoratedBox(
+                    decoration: BoxDecoration(color: Colors.black12));
+              }
+
+              return FadeIn(child: child);
+            },
+          ),
+        ),
+      ),
+    );
   }
 }
