838b86ed02ecb287b7303d760dcd21fcf03a6950
Jose Alonso <mail@josealonso.dev>
2023-08-11 00:14:41 -0600

    Cinemapedia: Custom navigation bar

--- Sec012/cinemapedia/lib/presentation/widgets/widgets.dart
+++ Sec012/cinemapedia/lib/presentation/widgets/widgets.dart
@@ -1,2 +1,3 @@
+export 'package:cinemapedia/presentation/widgets/shared/custom_bottom_navigation.dart';
 export 'package:cinemapedia/presentation/widgets/movies/movies_slideshow.dart';
 export 'package:cinemapedia/presentation/widgets/shared/custom_appbar.dart';
