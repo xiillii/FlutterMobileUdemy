e90f82bedc2f35167bb5df73bebcea30753827ef
Jose Alonso <mail@josealonso.dev>
2023-08-11 15:55:56 -0600

    Cinemapedia: Movie horizontal listview

--- Sec012/cinemapedia/lib/presentation/screens/movies/home_screen.dart
+++ Sec012/cinemapedia/lib/presentation/screens/movies/home_screen.dart
@@ -34,11 +34,17 @@ class _HomeViewState extends ConsumerState<_HomeView> {
 
   @override
   Widget build(BuildContext context) {
+    final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
     final slideShowMovies = ref.watch(moviesSlideshowProvider);
 
     return Column(children: [
       const CustomAppbar(),
       MoviesSlideshow(movies: slideShowMovies),
+      MovieHorizontalListview(
+        movies: nowPlayingMovies,
+        title: 'In Theaters',
+        subtitle: 'Today',
+      ),
     ]);
   }
 }
