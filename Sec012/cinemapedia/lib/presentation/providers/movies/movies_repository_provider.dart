56ffea434e66a4fa338524553f4ee6fa69c5bb25
Jose Alonso <mail@josealonso.dev>
2023-08-10 21:43:05 -0600

    Cinemapedia: Repository provider with riverpod

---
+++ Sec012/cinemapedia/lib/presentation/providers/movies/movies_repository_provider.dart
@@ -0,0 +1,7 @@
+import 'package:cinemapedia/infrastructure/datasources/moviedb_datasource.dart';
+import 'package:cinemapedia/infrastructure/repositories/movie_repository_impl.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+// inmutable repository
+final movieRepositoryProvider =
+    Provider((ref) => MovieRepositoryImpl(MovieDbDatasource()));
