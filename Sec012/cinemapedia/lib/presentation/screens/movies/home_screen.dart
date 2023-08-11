97890658da9b2100fed54e503c4d5be7a9e1535d
Jose Alonso <mail@josealonso.dev>
2023-08-11 00:04:57 -0600

    Cinemapedia: Show only 6 movies in the carusell

--- Sec012/cinemapedia/lib/presentation/screens/movies/home_screen.dart
+++ Sec012/cinemapedia/lib/presentation/screens/movies/home_screen.dart
@@ -1,4 +1,4 @@
-import 'package:cinemapedia/presentation/providers/movies/movies_providers.dart';
+import 'package:cinemapedia/presentation/providers/providers.dart';
 import 'package:cinemapedia/presentation/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -33,10 +33,11 @@ class _HomeViewState extends ConsumerState<_HomeView> {
 
   @override
   Widget build(BuildContext context) {
-    final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
+    final slideShowMovies = ref.watch(moviesSlideshowProvider);
+
     return Column(children: [
       const CustomAppbar(),
-      MoviesSlideshow(movies: nowPlayingMovies),
+      MoviesSlideshow(movies: slideShowMovies),
     ]);
   }
 }
