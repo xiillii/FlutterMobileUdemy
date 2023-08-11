5e43742a8d1427c3e48703a277777ec3f3d6e24e
Jose Alonso <mail@josealonso.dev>
2023-08-10 20:12:22 -0600

    Cinemapedia: MovieRepository, implementation

---
+++ Sec012/cinemapedia/lib/infrastructure/repositories/movie_repository_impl.dart
@@ -0,0 +1,13 @@
+import 'package:cinemapedia/domain/datasources/movies_datasource.dart';
+import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/domain/repositories/movies_repository.dart';
+
+class MovieRepositoryImpl extends MovieRepository {
+  final MoviesDatasource datasource;
+
+  MovieRepositoryImpl(this.datasource);
+
+  @override
+  Future<List<Movie>> getNowPlaying({int page = 1}) =>
+      datasource.getNowPlaying(page: page);
+}
