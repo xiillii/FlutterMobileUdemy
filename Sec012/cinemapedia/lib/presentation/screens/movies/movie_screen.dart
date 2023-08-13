56e16036ce89e7b5980382a78dcac607c29ada6a
Jose Alonso <mail@josealonso.dev>
2023-08-12 21:23:29 -0600

    Cinemapedia: Movie details screen

--- Sec012/cinemapedia/lib/presentation/screens/movies/movie_screen.dart
+++ Sec012/cinemapedia/lib/presentation/screens/movies/movie_screen.dart
@@ -38,8 +38,90 @@ class _MovieScreenState extends ConsumerState<MovieScreen> {
     return Scaffold(
       body: CustomScrollView(
         physics: const ClampingScrollPhysics(),
-        slivers: [_CustomSliverAppBar(movie: movie)],
+        slivers: [
+          _CustomSliverAppBar(movie: movie),
+          SliverList(
+              delegate: SliverChildBuilderDelegate(
+                  (context, index) => _MovieDetails(
+                        movie: movie,
+                      ),
+                  childCount: 1))
+        ],
       ),
+    );
+  }
+}
+
+class _MovieDetails extends StatelessWidget {
+  final Movie movie;
+
+  const _MovieDetails({required this.movie});
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    final textStyles = Theme.of(context).textTheme;
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              //* Image
+              ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: Image.network(
+                  movie.posterPath,
+                  width: size.width * 0.3,
+                ),
+              ),
+              const SizedBox(
+                width: 10,
+              ),
+
+              //* Description
+              SizedBox(
+                width: (size.width - 40) * 0.7,
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        movie.title,
+                        style: textStyles.titleLarge,
+                      ),
+                      Text(movie.overview)
+                    ]),
+              )
+            ],
+          ),
+        ),
+
+        Padding(
+          padding: const EdgeInsets.all(8),
+          child: Wrap(
+            children: [
+              ...movie.genreIds.map((gender) => Container(
+                    margin: const EdgeInsets.only(right: 10),
+                    child: Chip(
+                      label: Text(gender),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20)),
+                    ),
+                  ))
+            ],
+          ),
+        ),
+
+        //TODO: show actors
+        const SizedBox(
+          height: 100,
+        ),
+
+        //TODO: Similar movies
+      ],
     );
   }
 }
