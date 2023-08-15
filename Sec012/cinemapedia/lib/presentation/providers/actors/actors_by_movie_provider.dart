82a7e72b072a353cc88900a5b7fabe2fd18712ed
Jose Alonso <mail@josealonso.dev>
2023-08-14 19:59:53 -0600

    Cinemapedia: Actors repository and provider implementations

---
+++ Sec012/cinemapedia/lib/presentation/providers/actors/actors_by_movie_provider.dart
@@ -0,0 +1,28 @@
+import 'package:cinemapedia/domain/entities/actor.dart';
+import 'package:cinemapedia/presentation/providers/actors/actors_repository_provider.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final actorsByMovieProvider =
+    StateNotifierProvider<ActorsByMovieNotifier, Map<String, List<Actor>>>(
+        (ref) {
+  final fetchActorsMovieById =
+      ref.watch(actorsRepositoryProvider).getActorsByMovie;
+
+  return ActorsByMovieNotifier(getActors: fetchActorsMovieById);
+});
+
+typedef GetActorsCallback = Future<List<Actor>> Function(String moveId);
+
+class ActorsByMovieNotifier extends StateNotifier<Map<String, List<Actor>>> {
+  final GetActorsCallback getActors;
+
+  ActorsByMovieNotifier({required this.getActors}) : super({});
+
+  Future<void> loadActors(String movieId) async {
+    if (state[movieId] != null) return;
+
+    final actors = await getActors(movieId);
+
+    state = {...state, movieId: actors};
+  }
+}
--- Sec012/cinemapedia/lib/presentation/providers/providers.dart
+++ Sec012/cinemapedia/lib/presentation/providers/providers.dart
@@ -3,3 +3,4 @@ export 'movies/movie_info_provider.dart';
 export 'movies/movies_providers.dart';
 export 'movies/movies_repository_provider.dart';
 export 'movies/movies_slideshow_provider.dart';
+export 'actors/actors_by_movie_provider.dart';
