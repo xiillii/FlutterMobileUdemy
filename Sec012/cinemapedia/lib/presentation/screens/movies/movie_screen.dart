c8ef82805baccfc8f96203f8f1d96f0f14599045
Jose Alonso <mail@josealonso.dev>
2023-08-14 20:15:28 -0600

    Cinemapedia: Remove movie title from detail screen

--- Sec012/cinemapedia/lib/presentation/screens/movies/movie_screen.dart
+++ Sec012/cinemapedia/lib/presentation/screens/movies/movie_screen.dart
@@ -141,11 +141,6 @@ class _CustomSliverAppBar extends StatelessWidget {
       foregroundColor: Colors.white,
       flexibleSpace: FlexibleSpaceBar(
         titlePadding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-        title: Text(
-          movie.title,
-          style: const TextStyle(fontSize: 20),
-          textAlign: TextAlign.start,
-        ),
         background: Stack(children: [
           SizedBox.expand(
             child: Image.network(
