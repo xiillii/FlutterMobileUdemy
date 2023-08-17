4c1417773a0ea9ed39e11ce60c17cae29ba7faa2
Jose Alonso <mail@josealonso.dev>
2023-08-16 23:34:44 -0600

    Cinemapedia: Search delegate screen Leading and Actions

--- Sec012/cinemapedia/lib/presentation/delegates/search_movie_delegate.dart
+++ Sec012/cinemapedia/lib/presentation/delegates/search_movie_delegate.dart
@@ -1,18 +1,29 @@
+import 'package:animate_do/animate_do.dart';
+import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:flutter/material.dart';
 
-class SearchMovieDelegate extends SearchDelegate {
+class SearchMovieDelegate extends SearchDelegate<Movie?> {
   @override
   String get searchFieldLabel => 'Search movie';
 
   @override
   List<Widget>? buildActions(BuildContext context) {
-    return [Text('BuildActions')];
+    return [
+      FadeIn(
+        animate: query.isNotEmpty,
+        duration: const Duration(milliseconds: 100),
+        child: IconButton(
+            onPressed: () => query = '',
+            icon: const Icon(Icons.clear_outlined)),
+      )
+    ];
   }
 
   @override
   Widget? buildLeading(BuildContext context) {
-    // TODO: implement buildLeading
-    return Text('BuildLeading');
+    return IconButton(
+        onPressed: () => close(context, null),
+        icon: const Icon(Icons.arrow_back_ios_new_outlined));
   }
 
   @override
