535943cdb0fa407f3bb84a76e935d361a0af987a
Jose Alonso <mail@josealonso.dev>
2023-08-22 01:27:15 -0600

    Cinemapedia: Initial Favorites View. Show only first 10 favorites movie title

--- Sec012/cinemapedia/lib/presentation/providers/providers.dart
+++ Sec012/cinemapedia/lib/presentation/providers/providers.dart
@@ -7,3 +7,4 @@ export 'movies/movies_slideshow_provider.dart';
 export 'movies/similar_movies_provider.dart';
 export 'search/search_movies_provider.dart';
 export 'storage/local_storage_provider.dart';
+export 'storage/favorite_movies_provider.dart';
--- Sec012/cinemapedia/lib/presentation/views/movies/favorites_view.dart
+++ Sec012/cinemapedia/lib/presentation/views/movies/favorites_view.dart
@@ -1,15 +1,38 @@
-import 'package:flutter/material.dart';
+import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/presentation/providers/providers.dart';
 
-class FavoritesView extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class FavoritesView extends ConsumerStatefulWidget {
   const FavoritesView({super.key});
 
   @override
+  FavoritesViewState createState() => FavoritesViewState();
+}
+
+class FavoritesViewState extends ConsumerState<FavoritesView> {
+  @override
+  void initState() {
+    super.initState();
+    ref.read(favoriteMoviesProvider.notifier).loadNextPage();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final favoriteMovies = ref.watch(favoriteMoviesProvider);
+    List<Movie> movies = favoriteMovies.values.toList();
+
     return Scaffold(
       appBar: AppBar(title: const Text('Favorires View')),
-      body: const Center(
-        child: Text('Favorites'),
-      ),
+      body: ListView.builder(
+          itemCount: favoriteMovies.length,
+          itemBuilder: (context, index) {
+            final movie = movies[index];
+            return ListTile(
+              title: Text(movie.title),
+            );
+          }),
     );
   }
 }
