dd56daaa9830eebc31b0e097bbd99be2e23dd921
Jose Alonso <mail@josealonso.dev>
2023-08-21 19:14:09 -0600

    Cinemapedia: Change home screen with Views

--- Sec012/cinemapedia/lib/config/router/app_router.dart
+++ Sec012/cinemapedia/lib/config/router/app_router.dart
@@ -1,11 +1,14 @@
 import 'package:cinemapedia/presentation/screens/screens.dart';
+import 'package:cinemapedia/presentation/views/views.dart';
 import 'package:go_router/go_router.dart';
 
 final appRouter = GoRouter(initialLocation: '/', routes: [
   GoRoute(
       path: '/',
       name: HomeScreen.name,
-      builder: (context, state) => const HomeScreen(),
+      builder: (context, state) => const HomeScreen(
+            childView: HomeView(),
+          ),
       routes: [
         GoRoute(
           path: 'movie/:id',
