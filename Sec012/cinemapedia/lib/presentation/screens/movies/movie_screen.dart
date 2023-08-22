a24f2206aabadd1955a69f20626d02d6e59c556e
Jose Alonso <mail@josealonso.dev>
2023-08-22 00:07:12 -0600

    Cinemapedia: Family and Autodispose provider for favorite icon

--- Sec012/cinemapedia/lib/presentation/screens/movies/movie_screen.dart
+++ Sec012/cinemapedia/lib/presentation/screens/movies/movie_screen.dart
@@ -241,12 +241,21 @@ class _ActorsByMovie extends ConsumerWidget {
   }
 }
 
+final isFavoriteProvider =
+    FutureProvider.family.autoDispose((ref, int movieId) {
+  final repository = ref.watch(localStorageRepositoryProvider);
+
+  return repository.isMovieFavorite(movieId);
+});
+
 class _CustomSliverAppBar extends ConsumerWidget {
   final Movie movie;
   const _CustomSliverAppBar({required this.movie});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final isFavoriteFuture = ref.watch(isFavoriteProvider(movie.id));
+
     final size = MediaQuery.of(context).size;
 
     return SliverAppBar(
@@ -255,15 +264,25 @@ class _CustomSliverAppBar extends ConsumerWidget {
       foregroundColor: Colors.white,
       actions: [
         IconButton(
-            onPressed: () {
-              ref.watch(localStorageRepositoryProvider).toggleFavorite(movie);
-            },
-            icon: const Icon(Icons.favorite_outline)
-            // icon: const Icon(
-            //   Icons.favorite_rounded,
-            //   color: Colors.red,
-            // ),
-            )
+          onPressed: () async {
+            await ref
+                .watch(localStorageRepositoryProvider)
+                .toggleFavorite(movie);
+
+            ref.invalidate(isFavoriteProvider);
+          },
+          icon: isFavoriteFuture.when(
+              data: (isFavorite) => isFavorite
+                  ? const Icon(
+                      Icons.favorite_rounded,
+                      color: Colors.red,
+                    )
+                  : const Icon(Icons.favorite_outline),
+              error: (_, __) => throw UnimplementedError(),
+              loading: () => const CircularProgressIndicator(
+                    strokeWidth: 2,
+                  )),
+        )
       ],
       flexibleSpace: FlexibleSpaceBar(
         titlePadding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
