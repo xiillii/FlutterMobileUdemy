00cf91f20de33210afc948af10e89429d2673b09
Jose Alonso <mail@josealonso.dev>
2023-08-21 20:38:45 -0600

    Cinemapedia: Configure GoRoute

--- Sec012/cinemapedia/lib/presentation/screens/movies/home_screen.dart
+++ Sec012/cinemapedia/lib/presentation/screens/movies/home_screen.dart
@@ -4,14 +4,24 @@ import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
   static const name = 'home-screen';
+  final int pageIndex;
 
-  const HomeScreen({super.key});
+  const HomeScreen({super.key, required this.pageIndex});
+
+  final viewRoutes = const <Widget>[
+    HomeView(),
+    CategoriesView(),
+    FavoritesView()
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: HomeView(),
-      bottomNavigationBar: CustomBottomNavigation(),
+    return Scaffold(
+      body: IndexedStack(
+        index: pageIndex,
+        children: viewRoutes,
+      ),
+      bottomNavigationBar: const CustomBottomNavigation(),
     );
   }
 }
