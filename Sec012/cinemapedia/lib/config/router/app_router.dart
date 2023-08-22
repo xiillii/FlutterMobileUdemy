958c3bf2eca38a7e819f238f6ef4701401963c39
Jose Alonso <mail@josealonso.dev>
2023-08-21 19:32:23 -0600

    Cinemapedia: Change GoRouter by ShellRoute

--- Sec012/cinemapedia/lib/config/router/app_router.dart
+++ Sec012/cinemapedia/lib/config/router/app_router.dart
@@ -3,23 +3,28 @@ import 'package:cinemapedia/presentation/views/views.dart';
 import 'package:go_router/go_router.dart';
 
 final appRouter = GoRouter(initialLocation: '/', routes: [
-  GoRoute(
-      path: '/',
-      name: HomeScreen.name,
-      builder: (context, state) => const HomeScreen(
-            childView: HomeView(),
-          ),
+  ShellRoute(
+      builder: (context, state, child) {
+        return HomeScreen(childView: child);
+      },
       routes: [
         GoRoute(
-          path: 'movie/:id',
-          name: MovieScreen.name,
-          builder: (context, state) {
-            final movieId = state.pathParameters['id'] ?? 'no-id';
+            path: '/',
+            builder: (context, state) => const HomeView(),
+            routes: [
+              GoRoute(
+                path: 'movie/:id',
+                name: MovieScreen.name,
+                builder: (context, state) {
+                  final movieId = state.pathParameters['id'] ?? 'no-id';
 
-            return MovieScreen(
-              movieId: movieId,
-            );
-          },
-        )
-      ]),
+                  return MovieScreen(movieId: movieId);
+                },
+              )
+            ]),
+        GoRoute(
+          path: '/favorites',
+          builder: (context, state) => const FavoritesView(),
+        ),
+      ])
 ]);
