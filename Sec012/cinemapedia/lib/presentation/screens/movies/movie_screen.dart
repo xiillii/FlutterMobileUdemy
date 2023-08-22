212fada87854f86eb6dbbfb81dfc014b48fa9d26
Jose Alonso <mail@josealonso.dev>
2023-08-21 22:08:53 -0600

    Cinemapedia: Favorite button in screen details

--- Sec012/cinemapedia/lib/presentation/screens/movies/movie_screen.dart
+++ Sec012/cinemapedia/lib/presentation/screens/movies/movie_screen.dart
@@ -253,6 +253,18 @@ class _CustomSliverAppBar extends StatelessWidget {
       backgroundColor: Colors.black,
       expandedHeight: size.height * 0.7,
       foregroundColor: Colors.white,
+      actions: [
+        IconButton(
+            onPressed: () {
+              // TODO: Toggle favorite
+            },
+            icon: const Icon(Icons.favorite_outline)
+            // icon: const Icon(
+            //   Icons.favorite_rounded,
+            //   color: Colors.red,
+            // ),
+            )
+      ],
       flexibleSpace: FlexibleSpaceBar(
         titlePadding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
         background: Stack(children: [
@@ -269,28 +281,45 @@ class _CustomSliverAppBar extends StatelessWidget {
                   const ImageUrlError(),
             ),
           ),
-          const SizedBox.expand(
-            child: DecoratedBox(
-                decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                        begin: Alignment.topCenter,
-                        end: Alignment.bottomCenter,
-                        stops: [0.7, 1.0],
-                        colors: [Colors.transparent, Colors.black87]))),
+          const _CustomGradient(
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+            stops: [0.0, 0.2],
+            colors: [Colors.black54, Colors.transparent],
           ),
-          const SizedBox.expand(
-            child: DecoratedBox(
-                decoration: BoxDecoration(
-                    gradient: LinearGradient(begin: Alignment.topLeft, stops: [
-              0.0,
-              0.3
-            ], colors: [
-              Colors.black87,
-              Colors.transparent,
-            ]))),
-          ),
+          const _CustomGradient(begin: Alignment.topLeft, stops: [
+            0.0,
+            0.3
+          ], colors: [
+            Colors.black87,
+            Colors.transparent,
+          ]),
         ]),
       ),
+    );
+  }
+}
+
+class _CustomGradient extends StatelessWidget {
+  final AlignmentGeometry begin;
+  final AlignmentGeometry end;
+  final List<double> stops;
+  final List<Color> colors;
+
+  const _CustomGradient({
+    this.begin = Alignment.centerLeft,
+    this.end = Alignment.centerRight,
+    required this.stops,
+    required this.colors,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox.expand(
+      child: DecoratedBox(
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  begin: begin, end: end, stops: stops, colors: colors))),
     );
   }
 }
