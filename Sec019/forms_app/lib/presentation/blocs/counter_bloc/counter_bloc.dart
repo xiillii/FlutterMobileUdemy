67e784214aa169357a75e7b86b415c1bc5727801
Jose Alonso <mail@josealonso.dev>
2023-10-02 21:33:25 -0600

    FormsApp. Simplify BLoC handler

--- Sec019/forms_app/lib/presentation/blocs/counter_bloc/counter_bloc.dart
+++ Sec019/forms_app/lib/presentation/blocs/counter_bloc/counter_bloc.dart
@@ -6,10 +6,12 @@ part 'counter_state.dart';
 
 class CounterBloc extends Bloc<CounterEvent, CounterState> {
   CounterBloc() : super(const CounterState()) {
-    on<CounterIncreased>((event, emit) {
-      emit(state.copyWith(
-          counter: state.counter + event.value,
-          transactionCount: state.transactionCount + 1));
-    });
+    on<CounterIncreased>(_onCounterIncreased);
+  }
+
+  void _onCounterIncreased(CounterIncreased event, Emitter<CounterState> emit) {
+    emit(state.copyWith(
+        counter: state.counter + event.value,
+        transactionCount: state.transactionCount + 1));
   }
 }
