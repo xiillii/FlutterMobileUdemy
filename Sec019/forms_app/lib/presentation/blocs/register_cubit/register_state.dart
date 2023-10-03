6b6fc689e45780871fe2b11d858354630a385eef
Jose Alonso <mail@josealonso.dev>
2023-10-03 00:11:33 -0600

    FormsApp. Register Form Cubit

---
+++ Sec019/forms_app/lib/presentation/blocs/register_cubit/register_state.dart
@@ -0,0 +1,32 @@
+part of 'register_cubit.dart';
+
+enum FormStatus { invalid, valid, validating, posting }
+
+class RegisterFormState extends Equatable {
+  final FormStatus formStatus;
+  final String username;
+  final String email;
+  final String password;
+
+  const RegisterFormState(
+      {this.formStatus = FormStatus.invalid,
+      this.username = '',
+      this.email = '',
+      this.password = ''});
+
+  RegisterFormState copyWith({
+    FormStatus? formStatus,
+    String? username,
+    String? email,
+    String? password,
+  }) =>
+      RegisterFormState(
+        formStatus: formStatus ?? this.formStatus,
+        username: username ?? this.username,
+        email: email ?? this.email,
+        password: password ?? this.password,
+      );
+
+  @override
+  List<Object> get props => [formStatus, username, email, password];
+}
