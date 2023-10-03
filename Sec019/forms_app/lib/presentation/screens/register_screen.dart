30971a5a5e1c67fc4b4a05cf9fc001069c4d87a9
Jose Alonso <mail@josealonso.dev>
2023-10-02 23:17:17 -0600

    FormsApp. Initial Custom Form Field design

--- Sec019/forms_app/lib/presentation/screens/register_screen.dart
+++ Sec019/forms_app/lib/presentation/screens/register_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:forms_app/presentation/widgets/widgets.dart';
 
 class RegisterScreen extends StatelessWidget {
   const RegisterScreen({super.key});
@@ -17,34 +18,45 @@ class _RegisterView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
+    return const SafeArea(
       child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 10),
+        padding: EdgeInsets.symmetric(horizontal: 10),
         child: SingleChildScrollView(
           child: Column(children: [
-            const FlutterLogo(
+            FlutterLogo(
               size: 500,
             ),
-            TextFormField(),
-            TextFormField(),
-            TextFormField(),
-            TextFormField(),
-            TextFormField(),
-            TextFormField(),
-            TextFormField(),
-            const SizedBox(
-              height: 20,
-            ),
-            FilledButton.tonalIcon(
-                onPressed: () {},
-                icon: const Icon(Icons.save),
-                label: const Text('Save')),
-            const SizedBox(
+            _RegisterForm(),
+            SizedBox(
               height: 20,
             )
           ]),
         ),
       ),
+    );
+  }
+}
+
+class _RegisterForm extends StatelessWidget {
+  const _RegisterForm();
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      child: Column(children: [
+        CustomTextFormField(),
+        const SizedBox(
+          height: 10,
+        ),
+        CustomTextFormField(),
+        const SizedBox(
+          height: 20,
+        ),
+        FilledButton.tonalIcon(
+            onPressed: () {},
+            icon: const Icon(Icons.save),
+            label: const Text('Save')),
+      ]),
     );
   }
 }
---
+++ Sec019/forms_app/lib/presentation/widgets/inputs/custom_text_form_field.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class CustomTextFormField extends StatelessWidget {
+  const CustomTextFormField({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+    final border = OutlineInputBorder(borderRadius: BorderRadius.circular(40));
+
+    return TextFormField(
+      onChanged: (value) {
+        print('value: ${value}');
+      },
+      validator: (value) {
+        return 'Error en el formuilario';
+      },
+      decoration: InputDecoration(
+          enabledBorder: border,
+          focusedBorder:
+              border.copyWith(borderSide: BorderSide(color: colors.primary)),
+          errorBorder: border.copyWith(
+              borderSide: BorderSide(color: Colors.red.shade800)),
+          focusedErrorBorder: border.copyWith(
+              borderSide: BorderSide(color: Colors.red.shade800)),
+          isDense: true,
+          label: Text('The Label'),
+          hintText: 'This is the hint text',
+          errorText: 'This is the error text',
+          focusColor: colors.primary),
+    );
+  }
+}
