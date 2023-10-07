a43fdf840e87a81bce4b6aa5a347b123aedbd5ad
Jose Alonso <mail@josealonso.dev>
2023-10-07 00:40:14 -0600

    PushApp. Convert the notification push to an Entity

--- Sec021/push_app/lib/presentation/blocs/notifications/notifications_bloc.dart
+++ Sec021/push_app/lib/presentation/blocs/notifications/notifications_bloc.dart
@@ -1,7 +1,10 @@
+import 'dart:io';
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:push_app/domain/entities/push_message.dart';
 import 'package:push_app/firebase_options.dart';
 
 part 'notifications_event.dart';
@@ -53,12 +56,20 @@ class NotificationsBloc extends Bloc<NotificationsEvent, NotificationsState> {
   }
 
   void _handleRemoteMessage(RemoteMessage message) {
-    print('Got a message whilst in the foreground!');
-    print('Message data: ${message.data}');
+    if (message.notification == null) return;
 
-    if (message.notification != null) {
-      print('Message also contained a notification: ${message.notification}');
-    }
+    final notification = PushMessage(
+        messageId:
+            message.messageId?.replaceAll(':', '').replaceAll('%', '') ?? '',
+        title: message.notification!.title ?? '',
+        body: message.notification!.body ?? '',
+        sentDate: message.sentTime ?? DateTime.now(),
+        data: message.data,
+        imageUrl: Platform.isAndroid
+            ? message.notification!.android?.imageUrl
+            : message.notification!.apple?.imageUrl);
+
+    print(notification);
   }
 
   Future requestPermission() async {
--- Sec021/push_app/lib/presentation/blocs/notifications/notifications_state.dart
+++ Sec021/push_app/lib/presentation/blocs/notifications/notifications_state.dart
@@ -2,14 +2,14 @@ part of 'notifications_bloc.dart';
 
 class NotificationsState extends Equatable {
   final AuthorizationStatus status;
-  final List<dynamic> notifications;
+  final List<PushMessage> notifications;
 
   const NotificationsState(
       {this.status = AuthorizationStatus.notDetermined,
       this.notifications = const []});
 
   NotificationsState copyWith(
-          {AuthorizationStatus? status, List<dynamic>? notifications}) =>
+          {AuthorizationStatus? status, List<PushMessage>? notifications}) =>
       NotificationsState(
           status: status ?? this.status,
           notifications: notifications ?? this.notifications);
