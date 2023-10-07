c2161ac22d194f6d07cf86e209694f285ab0af20
Jose Alonso <mail@josealonso.dev>
2023-10-06 22:07:11 -0600

    PushApp. Initialize Firebase in Flutter

--- Sec021/push_app/lib/main.dart
+++ Sec021/push_app/lib/main.dart
@@ -4,7 +4,10 @@ import 'package:push_app/config/router/app_router.dart';
 import 'package:push_app/config/theme/app_theme.dart';
 import 'package:push_app/presentation/blocs/notifications/notifications_bloc.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await NotificationsBloc.initializeFirebaseNotifications();
+
   runApp(MultiBlocProvider(
       providers: [BlocProvider(create: (_) => NotificationsBloc())],
       child: const MainApp()));
--- Sec021/push_app/lib/presentation/blocs/notifications/notifications_bloc.dart
+++ Sec021/push_app/lib/presentation/blocs/notifications/notifications_bloc.dart
@@ -1,6 +1,8 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:push_app/firebase_options.dart';
 
 part 'notifications_event.dart';
 part 'notifications_state.dart';
@@ -12,6 +14,11 @@ class NotificationsBloc extends Bloc<NotificationsEvent, NotificationsState> {
     // on<NotificationsEvent>((event, emit) {
     //   // TODO: implement event handler
     // });
+  }
+
+  static Future<void> initializeFirebaseNotifications() async {
+    await Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform);
   }
 
   Future requestPermission() async {
