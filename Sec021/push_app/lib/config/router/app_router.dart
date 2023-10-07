4f2363ba4b654f7f581f8efef87d85b3a70ac0f5
Jose Alonso <mail@josealonso.dev>
2023-10-07 02:04:43 -0600

    PushApp. Interact with the notifications
    Show screen details

--- Sec021/push_app/lib/config/router/app_router.dart
+++ Sec021/push_app/lib/config/router/app_router.dart
@@ -1,9 +1,16 @@
 import 'package:go_router/go_router.dart';
+import 'package:push_app/presentation/screens/details_screen.dart';
 import 'package:push_app/presentation/screens/home_screen.dart';
 
 final appRouter = GoRouter(routes: [
   GoRoute(
     path: '/',
     builder: (_, __) => const HomeScreen(),
-  )
+  ),
+  GoRoute(
+    path: '/push-details/:messageId',
+    builder: (context, state) => DetailsScreen(
+      pushMessageId: state.pathParameters['messageId'] ?? '',
+    ),
+  ),
 ]);
--- Sec021/push_app/lib/presentation/screens/home_screen.dart
+++ Sec021/push_app/lib/presentation/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
 import 'package:push_app/presentation/blocs/notifications/notifications_bloc.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -37,11 +38,15 @@ class _HomeView extends StatelessWidget {
       itemBuilder: (context, index) {
         final notification = notifications[index];
         return ListTile(
-            title: Text(notification.title),
-            subtitle: Text(notification.body),
-            leading: notification.imageUrl != null
-                ? Image.network(notification.imageUrl!)
-                : null);
+          title: Text(notification.title),
+          subtitle: Text(notification.body),
+          leading: notification.imageUrl != null
+              ? Image.network(notification.imageUrl!)
+              : null,
+          onTap: () {
+            context.push('/push-details/${notification.messageId}');
+          },
+        );
       },
     );
   }
