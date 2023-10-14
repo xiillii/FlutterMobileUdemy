10fcd526dcdf3737f8406d3b0ba3950f1236feaf
Jose Alonso <mail@josealonso.dev>
2023-10-14 00:01:06 -0600

    TesloApp. Fix Key value storage service

--- Sec026/teslo_app/lib/features/shared/infrastructure/services/key_value_storage_service.dart
+++ Sec026/teslo_app/lib/features/shared/infrastructure/services/key_value_storage_service.dart
@@ -1,5 +1,5 @@
 abstract class KeyValueStorageService {
   Future<void> setKeyValue(String key, dynamic value);
-  Future getValue(String key, dynamic value);
+  Future getValue(String key);
   Future<bool> removeKey(String key);
 }
