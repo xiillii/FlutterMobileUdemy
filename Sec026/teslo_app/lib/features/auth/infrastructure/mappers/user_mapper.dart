8b8f91e545ed532ec1ccc22fb735a7702dc2cc6b
Jose Alonso <mail@josealonso.dev>
2023-10-13 23:13:57 -0600

    TesloApp. Fix User mapper json data

--- Sec026/teslo_app/lib/features/auth/infrastructure/mappers/user_mapper.dart
+++ Sec026/teslo_app/lib/features/auth/infrastructure/mappers/user_mapper.dart
@@ -3,6 +3,6 @@ import 'dart:convert';
 import 'package:teslo_app/features/auth/domain/domain.dart';
 
 class UserMapper {
-  String toJson(User data) => json.encode(data.toJson());
-  User fromJson(String str) => User.fromJson(json.decode(str));
+  static String toJson(User data) => json.encode(data.toJson());
+  static User fromJson(Map<String, dynamic> data) => User.fromJson(data);
 }
