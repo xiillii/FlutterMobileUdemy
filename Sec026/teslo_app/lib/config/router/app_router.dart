e95c93b57c920179dcd40cef8675c2a8cf3223ec
Jose Alonso <mail@josealonso.dev>
2023-10-15 12:56:10 -0600

    TesloApp. Navigatin, checking authentication status

--- Sec026/teslo_app/lib/config/router/app_router.dart
+++ Sec026/teslo_app/lib/config/router/app_router.dart
@@ -1,14 +1,16 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:teslo_app/config/router/app_router_notifier.dart';
+
 import 'package:teslo_app/features/auth/auth.dart';
+import 'package:teslo_app/features/auth/presentation/providers/auth_provider.dart';
 import 'package:teslo_app/features/products/products.dart';
 
 final goRouterProvider = Provider((ref) {
   final goRouterNotifier = ref.read(goRouterNotifierProvider);
 
   return GoRouter(
-    initialLocation: '/login',
+    initialLocation: '/splash',
     refreshListenable: goRouterNotifier,
     routes: [
       ///* Auth Routes
@@ -32,7 +34,28 @@ final goRouterProvider = Provider((ref) {
       ),
     ],
     redirect: (context, state) {
-      print(state);
+      final isGoingTo = state.matchedLocation;
+      final authStatus = goRouterNotifier.authStatus;
+
+      if (isGoingTo == "/splash" && authStatus == AuthStatus.checking) {
+        return null;
+      }
+
+      if (authStatus == AuthStatus.notAuthenticated) {
+        if (isGoingTo == '/login' || isGoingTo == '/register') {
+          return null;
+        }
+
+        return '/login';
+      }
+
+      if (authStatus == AuthStatus.authenticated) {
+        if (isGoingTo == '/login' ||
+            isGoingTo == '/register' ||
+            isGoingTo == '/splash') {
+          return '/';
+        }
+      }
 
       return null;
     },
