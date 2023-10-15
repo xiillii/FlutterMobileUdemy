4868d3829b5a8a177e7f3155264bebeec0d172ee
Jose Alonso <mail@josealonso.dev>
2023-10-15 11:32:32 -0600

    TesloApp. GoRouterNotifier

--- Sec026/teslo_app/lib/config/router/app_router.dart
+++ Sec026/teslo_app/lib/config/router/app_router.dart
@@ -1,11 +1,15 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+import 'package:teslo_app/config/router/app_router_notifier.dart';
 import 'package:teslo_app/features/auth/auth.dart';
 import 'package:teslo_app/features/products/products.dart';
 
 final goRouterProvider = Provider((ref) {
+  final goRouterNotifier = ref.read(goRouterNotifierProvider);
+
   return GoRouter(
-    initialLocation: '/splash',
+    initialLocation: '/login',
+    refreshListenable: goRouterNotifier,
     routes: [
       ///* Auth Routes
       GoRoute(
