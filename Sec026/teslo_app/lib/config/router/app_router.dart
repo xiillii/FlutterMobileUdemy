f96781635a7816b2b51a659b38e6b04a01b2d44a
Jose Alonso <mail@josealonso.dev>
2023-10-14 18:38:15 -0600

    TesloApp. Route Provider

--- Sec026/teslo_app/lib/config/router/app_router.dart
+++ Sec026/teslo_app/lib/config/router/app_router.dart
@@ -1,30 +1,36 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:teslo_app/features/auth/auth.dart';
 import 'package:teslo_app/features/products/products.dart';
 
-final appRouter = GoRouter(
-  initialLocation: '/splash',
-  routes: [
-    ///* Auth Routes
-    GoRoute(
-      path: '/splash',
-      builder: (context, state) => const CheckAuthStatusScreen(),
-    ),
-    GoRoute(
-      path: '/login',
-      builder: (context, state) => const LoginScreen(),
-    ),
-    GoRoute(
-      path: '/register',
-      builder: (context, state) => const RegisterScreen(),
-    ),
+final goRouterProvider = Provider((ref) {
+  return GoRouter(
+    initialLocation: '/splash',
+    routes: [
+      ///* Auth Routes
+      GoRoute(
+        path: '/splash',
+        builder: (context, state) => const CheckAuthStatusScreen(),
+      ),
+      GoRoute(
+        path: '/login',
+        builder: (context, state) => const LoginScreen(),
+      ),
+      GoRoute(
+        path: '/register',
+        builder: (context, state) => const RegisterScreen(),
+      ),
 
-    ///* Product Routes
-    GoRoute(
-      path: '/',
-      builder: (context, state) => const ProductsScreen(),
-    ),
-  ],
+      ///* Product Routes
+      GoRoute(
+        path: '/',
+        builder: (context, state) => const ProductsScreen(),
+      ),
+    ],
+    redirect: (context, state) {
+      print(state);
 
-  ///! TODO: Bloquear si no se está autenticado de alguna manera
-);
+      return null;
+    },
+  );
+});
