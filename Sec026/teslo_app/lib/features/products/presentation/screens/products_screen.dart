3e9fa0cb56d21b65bb3b8c508c688f5888e2e91e
Jose Alonso <mail@josealonso.dev>
2023-10-17 22:36:51 -0600

    TesloApp. Masonry  control and products screen

--- Sec026/teslo_app/lib/features/products/presentation/screens/products_screen.dart
+++ Sec026/teslo_app/lib/features/products/presentation/screens/products_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:teslo_app/features/products/presentation/providers/providers.dart';
 import 'package:teslo_app/features/shared/shared.dart';
 
 class ProductsScreen extends StatelessWidget {
@@ -26,11 +29,47 @@ class ProductsScreen extends StatelessWidget {
   }
 }
 
-class _ProductsView extends StatelessWidget {
+class _ProductsView extends ConsumerStatefulWidget {
   const _ProductsView();
 
   @override
+  _ProductsViewState createState() => _ProductsViewState();
+}
+
+class _ProductsViewState extends ConsumerState<_ProductsView> {
+  final ScrollController scrollController = ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+    // TODO: infite scroll
+    ref.read(productsProvider.notifier).loadNextPage();
+  }
+
+  @override
+  void dispose() {
+    scrollController.dispose();
+
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Center(child: Text('Eres genial!'));
+    final productsState = ref.watch(productsProvider);
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      child: MasonryGridView.count(
+          physics: const BouncingScrollPhysics(),
+          crossAxisCount: 2,
+          mainAxisSpacing: 20,
+          crossAxisSpacing: 35,
+          itemCount: productsState.products.length,
+          itemBuilder: (context, index) {
+            final product = productsState.products[index];
+
+            return Text(product.title);
+          }),
+    );
   }
 }
