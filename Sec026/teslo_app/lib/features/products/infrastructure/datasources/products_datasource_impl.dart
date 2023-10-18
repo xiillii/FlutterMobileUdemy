de20dc5bbc3caf7183101d5a5af026c1fec91a6c
Jose Alonso <mail@josealonso.dev>
2023-10-17 18:34:17 -0600

    TesloApp. Implement getProductsByPage. Products datasource

--- Sec026/teslo_app/lib/features/products/infrastructure/datasources/products_datasource_impl.dart
+++ Sec026/teslo_app/lib/features/products/infrastructure/datasources/products_datasource_impl.dart
@@ -1,7 +1,17 @@
+import 'package:dio/dio.dart';
+import 'package:teslo_app/config/constants/environment.dart';
 import 'package:teslo_app/features/products/domain/datasources/products_datasources.dart';
 import 'package:teslo_app/features/products/domain/entities/product.dart';
 
 class ProductsDatasourceImpl implements ProductsDatasource {
+  late final Dio dio;
+  final String accessToken;
+
+  ProductsDatasourceImpl({required this.accessToken})
+      : dio = Dio(BaseOptions(
+            baseUrl: Environment.apiUrl,
+            headers: {'Authorization': 'Bearer $accessToken'}));
+
   @override
   Future<Product> createUpdateProduct(Map<String, dynamic> productLike) {
     // TODO: implement createUpdateProduct
@@ -15,9 +25,17 @@ class ProductsDatasourceImpl implements ProductsDatasource {
   }
 
   @override
-  Future<List<Product>> getProductsByPage({int limit = 10, int offset = 0}) {
-    // TODO: implement getProductsByPage
-    throw UnimplementedError();
+  Future<List<Product>> getProductsByPage(
+      {int limit = 10, int offset = 0}) async {
+    final response = await dio.get('/api/procucts',
+        queryParameters: {'limit': limit, 'offset': offset});
+    final List<Product> products = [];
+
+    for (final product in response.data ?? []) {
+      products.add(product);
+    }
+
+    return products;
   }
 
   @override
