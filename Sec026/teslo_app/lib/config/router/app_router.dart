04a03b33b4f1bd1a0751e167845f511db744c21f
Jose Alonso <mail@josealonso.dev>
2023-10-19 20:32:22 -0600

    TesloApp. Product screen for details. Initial design

--- Sec026/teslo_app/lib/config/router/app_router.dart
+++ Sec026/teslo_app/lib/config/router/app_router.dart
@@ -32,6 +32,12 @@ final goRouterProvider = Provider((ref) {
         path: '/',
         builder: (context, state) => const ProductsScreen(),
       ),
+      GoRoute(
+        path: '/product/:id',
+        builder: (context, state) => ProductScreen(
+          productId: state.pathParameters['id'] ?? 'no-id',
+        ),
+      ),
     ],
     redirect: (context, state) {
       final isGoingTo = state.matchedLocation;
--- Sec026/teslo_app/lib/features/products/presentation/screens/products_screen.dart
+++ Sec026/teslo_app/lib/features/products/presentation/screens/products_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:go_router/go_router.dart';
 import 'package:teslo_app/features/products/presentation/providers/providers.dart';
 import 'package:teslo_app/features/products/presentation/widgets/widgets.dart';
 import 'package:teslo_app/features/shared/shared.dart';
@@ -75,7 +76,9 @@ class _ProductsViewState extends ConsumerState<_ProductsView> {
           itemBuilder: (context, index) {
             final product = productsState.products[index];
 
-            return ProductCard(product: product);
+            return GestureDetector(
+                onTap: () => context.push('/product/${product.id}'),
+                child: ProductCard(product: product));
           }),
     );
   }
