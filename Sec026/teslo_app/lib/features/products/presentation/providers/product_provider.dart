dfc1f086948121fc8f1926d8d6eefad6264c9eb3
Jose Alonso <mail@josealonso.dev>
2023-10-19 21:05:24 -0600

    TesloApp. Product provider

---
+++ Sec026/teslo_app/lib/features/products/presentation/providers/product_provider.dart
@@ -0,0 +1,46 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:teslo_app/features/products/domain/entities/product.dart';
+import 'package:teslo_app/features/products/domain/repositories/products_repository.dart';
+import 'package:teslo_app/features/products/presentation/providers/providers.dart';
+
+final productProvider = StateNotifierProvider.autoDispose
+    .family<ProductNotifier, ProductState, String>((ref, productId) {
+  final productsRepository = ref.watch(productsRepositoryProvider);
+
+  return ProductNotifier(
+      productsRepository: productsRepository, productId: productId);
+});
+
+class ProductNotifier extends StateNotifier<ProductState> {
+  final ProductsRepository productsRepository;
+
+  ProductNotifier({required this.productsRepository, required String productId})
+      : super(ProductState(id: productId));
+
+  Future<void> loadProduct() async {}
+}
+
+class ProductState {
+  final String id;
+  final Product? product;
+  final bool isLoading;
+  final bool isSaving;
+
+  ProductState(
+      {required this.id,
+      this.product,
+      this.isLoading = true,
+      this.isSaving = false});
+
+  ProductState copyWith({
+    String? id,
+    Product? product,
+    bool? isLoading,
+    bool? isSaving,
+  }) =>
+      ProductState(
+          id: id ?? this.id,
+          product: product ?? this.product,
+          isLoading: isLoading ?? this.isLoading,
+          isSaving: isSaving ?? this.isSaving);
+}
