dbc7ae85e076d421f0e4db7af8d5570fe034189b
Jose Alonso <mail@josealonso.dev>
2023-10-20 21:10:34 -0600

    TesloApp. Product Form Provider. State

---
+++ Sec026/teslo_app/lib/features/products/presentation/providers/forms/product_form_provider.dart
@@ -0,0 +1,55 @@
+import 'package:teslo_app/features/shared/infrastructure/inputs/inputs.dart';
+
+class ProductFormState {
+  final bool isFormValid;
+  final String? id;
+  final Title title;
+  final Slug slug;
+  final Price price;
+  final List<String> sizes;
+  final String gender;
+  final Stock inStock;
+  final String description;
+  final String tags;
+  final List<String> images;
+
+  ProductFormState(
+      {this.isFormValid = false,
+      this.id,
+      this.title = const Title.dirty(''),
+      this.slug = const Slug.dirty(''),
+      this.price = const Price.dirty(0.0),
+      this.sizes = const [],
+      this.gender = 'women',
+      this.inStock = const Stock.dirty(0),
+      this.description = '',
+      this.tags = '',
+      this.images = const []});
+
+  ProductFormState copyWith({
+    bool? isFormValid,
+    String? id,
+    Title? title,
+    Slug? slug,
+    Price? price,
+    List<String>? sizes,
+    String? gender,
+    Stock? inStock,
+    String? description,
+    String? tags,
+    List<String>? images,
+  }) =>
+      ProductFormState(
+        isFormValid: isFormValid ?? this.isFormValid,
+        id: id ?? this.id,
+        title: title ?? this.title,
+        slug: slug ?? this.slug,
+        price: price ?? this.price,
+        sizes: sizes ?? this.sizes,
+        gender: gender ?? this.gender,
+        inStock: inStock ?? this.inStock,
+        description: description ?? this.description,
+        tags: tags ?? this.tags,
+        images: images ?? this.images,
+      );
+}
