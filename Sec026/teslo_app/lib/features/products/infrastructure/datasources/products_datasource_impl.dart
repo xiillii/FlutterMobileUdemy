71d1da8b92f7fb1b84b9e96ae00834928f5496ce
Jose Alonso <mail@josealonso.dev>
2023-10-21 00:20:56 -0600

    TestoApp. Product Datasource Implementation. Create or update method

--- Sec026/teslo_app/lib/features/products/infrastructure/datasources/products_datasource_impl.dart
+++ Sec026/teslo_app/lib/features/products/infrastructure/datasources/products_datasource_impl.dart
@@ -14,9 +14,24 @@ class ProductsDatasourceImpl implements ProductsDatasource {
             headers: {'Authorization': 'Bearer $accessToken'}));
 
   @override
-  Future<Product> createUpdateProduct(Map<String, dynamic> productLike) {
-    // TODO: implement createUpdateProduct
-    throw UnimplementedError();
+  Future<Product> createUpdateProduct(Map<String, dynamic> productLike) async {
+    try {
+      final String? productId = productLike['id'];
+      final String method = productId == null ? 'POST' : 'PATCH';
+      final String url =
+          productId == null ? '/products' : '/products/$productId';
+
+      productLike.remove('id');
+
+      final response = await dio.request(url,
+          data: productLike, options: Options(method: method));
+
+      final product = ProductMapper.jsonToEntity(response.data);
+
+      return product;
+    } catch (e) {
+      throw Exception();
+    }
   }
 
   @override
