6d49825903d35ec1ba902654c36a2c883570adc0
Jose Alonso <mail@josealonso.dev>
2023-10-20 23:44:00 -0600

    TesloApp. Connect Provider's OnSubmit to the details form

--- Sec026/teslo_app/lib/features/products/presentation/screens/product_screen.dart
+++ Sec026/teslo_app/lib/features/products/presentation/screens/product_screen.dart
@@ -25,7 +25,13 @@ class ProductScreen extends ConsumerWidget {
           ? const FullScreenLoader()
           : _ProductView(product: productState.product!),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          if (productState.product == null) return;
+
+          ref
+              .read(productFormProvider(productState.product!).notifier)
+              .onFormSubmit();
+        },
         child: const Icon(Icons.save_as_outlined),
       ),
     );
